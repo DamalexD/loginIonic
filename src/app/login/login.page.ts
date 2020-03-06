a7e8853ab8bc5d1f5eb9b5bae45166e8d7729bcc
DamalexD <damir_mor@hotmail.com>
2020-03-06 07:10:53 -0600

    Back con el usuario hard coreado

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+
+  clNombre:string;
+  clPass:string;
+  strMessage: string;
+  blnNext: boolean;
+  name: string;
+  
+
+  constructor(public alertController: AlertController, private router:Router) { }
+
+
+  async loguearse(strName:string, strPass:string){
+    this.strMessage = '';
+
+    (strName)? this.fnError() : this.fnError('Error: Podrías hacernos el favor a ambos de llenar el usuario');
+    (strPass)? this.fnError() : this.fnError('Error : ¿Eres tonto o no puedes poner tú contraseña?');
+
+    if(this.clNombre == "admin" && this.clPass == "123"){
+      this.router.navigate(['tabs/tab1']);
+    }else{
+      this.presentAlert();
+    }
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Un campo ingresado es inválido',
+      message: this.strMessage,
+      buttons: ['Deja le checo xD']
+    });
+
+    await alert.present();
+  }
+  
+  fnError(msg?: string){
+    if(msg){
+      //console.log(msg);
+      this.strMessage += '<br>' + msg + '<br>';
+      this.blnNext = true;
+    }else if(this.blnNext){
+      this.blnNext = true;
+    }else{
+      this.blnNext = false;
+    }
+  }
+
 
   ngOnInit() {
   }
+
+
+
 
 }
