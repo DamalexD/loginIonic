ff34a57106c6677129725e11072779378e3d1170
kassandra <kassandraavila02@gmail.com>
2020-04-18 16:21:19 -0500

    pipecreado

--- front/src/app/tab1/tab1.module.ts
+++ front/src/app/tab1/tab1.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { NumeroPipe } from '../pipes/numero.pipe';
 
 @NgModule({
   imports: [
@@ -14,6 +15,6 @@ import { ExploreContainerComponentModule } from '../explore-container/explore-co
     ExploreContainerComponentModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page, NumeroPipe]
 })
 export class Tab1PageModule {}
